2daa64a84a6de2abbcd0bb8a3befd3a1934e90bf
Adith-Sree-Jey <jeynadar23@gmail.com>
2025-07-24 16:42:28 +0530

    utils & logger added

---
+++ src/cnnClassifier/test.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from cnnClassifier import logging
+
+
+logging.info("welcome to our custom Log")
